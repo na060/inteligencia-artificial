dc787e56d2adece36244ad32d8e9f7937a9fdee2
nathieli garcia leite <110836585+na060@users.noreply.github.com>
2024-06-03 11:42:27 -0300

    aula3 parcial

---
+++ script.js
@@ -0,0 +1,5 @@
+const caixapincipal = document.querySelector(".caixa-principal");
+const caixaperguntas = document.querySelector(".caixa-perguntas");
+const caixaalternativa = document.querySelector(".caixa-alternativas");
+const caixaresultado = document.querySelector(".caixa-resultdo");
+const textoresultado =document.querySelector(".texto-resultado");
